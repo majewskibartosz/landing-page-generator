a6b89862e373663d3d3d53d6ad035cb2e3b8d0eb
majewskibartosz <majewskibartosz@pm.me>
2023-05-22 14:10:47 +0200

    add eslint and prettier + configs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
